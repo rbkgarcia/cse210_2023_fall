f940840b649f38bc883e975735d593aa15f72d90
rbkgarcia <gar19066@byui.edu>
2023-09-21 20:37:33 -0400

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string play = "yes";
+        while (play=="yes" || play=="y"){
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = 0;
+            int i=0;
+            while (guess!= magicNumber){
+                Console.WriteLine("What is the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+                i++;
+                if (guess>magicNumber){
+                    Console.WriteLine("Lower"); 
+                }
+                else if (guess<magicNumber){
+                    Console.WriteLine("Higher");
+                }
+                else{
+                    Console.WriteLine($"You guessed it! at {i} attempts");
+                }
+            }
+            Console.WriteLine("Do you want to play again? ");
+
+            play= Console.ReadLine();
+        }
     }
 }
